19e0e22cd28c057ecab55795840dcb246fd92e49
Addison Cole <addison.cole85@gmail.com>
2019-01-20 19:55:56 -0800

    create area chart for Gini Index data that is called with axios to back end to retrieve community survey data.

--- seattlization/src/components/DataContainer.js
+++ seattlization/src/components/DataContainer.js
@@ -5,8 +5,10 @@ import GraphItem from './GraphItem';
 import './DataContainer.css';
 
 const GET_COUNTS = "http://localhost:8000/homelesscounts/";
+const GET_SURVEYS = "http://localhost:8000/communitysurveys/";
 
-const YEARS = [2004, 2005, 2006, 2007, 2008, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018]
+const COUNT_YEARS = [2004, 2005, 2006, 2007, 2008, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018]
+const SURVEY_YEARS = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
 
 class DataContainer extends Component {
   constructor(props) {
@@ -14,6 +16,7 @@ class DataContainer extends Component {
 
     this.state = {
       yearlyHomelessCounts: [],
+      communitySurveys: [],
       statusText: '',
     }
   }
@@ -22,6 +25,11 @@ class DataContainer extends Component {
     this.setState({
       statusText: 'loading'
     });
+    this.getHomelessCounts()
+    this.getCommunitySurveys()
+  }
+
+  getHomelessCounts(){
     axios.get(GET_COUNTS)
     .then((response) => {
       this.setState({
@@ -37,15 +45,34 @@ class DataContainer extends Component {
     });
   }
 
+  getCommunitySurveys(){
+    axios.get(GET_SURVEYS)
+    .then((response) => {
+      this.setState({
+        communitySurveys: response.data,
+        statusText: 'success',
+      });
+    })
+    .catch((error) => {
+      console.log(error.response);
+      this.setState({
+        statusText: `${error.response}`
+      });
+    });
+  }
+
   render() {
 
-    const { yearlyHomelessCounts } = this.state;
+    const { yearlyHomelessCounts, communitySurveys } = this.state;
 
     const sortedHomelessCounts = yearlyHomelessCounts.sort(function(a, b){return a.year - b.year})
 
+    const sortedCommunitySurveys = communitySurveys.sort(function(a, b){return a.year - b.year})
+
     return (
       <div className="data-container">
-        <GraphItem type="bar-stacked" dataSet={sortedHomelessCounts} years={YEARS} xAxis="year" xAxisTitle="Years" yAxisTitle="# of Homeless in King County (in thousands)" datumOne="unsheltered" datumOneTitle="unsheltered" datumTwo="number_in_shelter_and_transitional_housing" datumTwoTitle="sheltered homeless"/>
+        <GraphItem type="bar-stacked" dataSet={sortedHomelessCounts} years={COUNT_YEARS} xAxis="year" xAxisTitle="Years" yAxisTitle="# of Homeless in King County (in thousands)" datumOne="unsheltered" datumOneTitle="unsheltered" datumTwo="number_in_shelter_and_transitional_housing" datumTwoTitle="sheltered homeless"/>
+        <GraphItem type="area-chart" dataSet={sortedCommunitySurveys} xAxis={"year"} yAxis={"gini_index"} xAxisTitle="Years" yAxisTitle="Gini Index" domain={[0.455, 0.475]}/>
       </div>
     )
   }
--- seattlization/src/components/GraphItem.js
+++ seattlization/src/components/GraphItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack, VictoryLabel, VictoryLegend, VictoryTheme} from 'victory'
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack, VictoryLabel, VictoryLegend, VictoryTheme, VictoryArea } from 'victory'
 import './GraphItem.css';
 
 // `${data.year.year}: Total:${data.year.total}, Unsheltered:${data.year.unsheltered} Sheltered:${data.year.sheltered} ;;  `
-const GraphItem = ({type, dataSet, years, xAxis, xAxisTitle, yAxisTitle, datumOne, datumOneTitle, datumTwo, datumTwoTitle}) => {
+const GraphItem = ({type, dataSet, years, xAxis, xAxisTitle, yAxis, yAxisTitle, datumOne, datumOneTitle, datumTwo, datumTwoTitle, domain}) => {
 
-  const filteredDataSet =  dataSet.filter(f => years.includes(f.year));
 
   if (type === "bar-stacked"){
+    const filteredDataSet =  dataSet.filter(f => years.includes(f.year));
     return (
       <div>
         <VictoryChart
@@ -17,32 +17,66 @@ const GraphItem = ({type, dataSet, years, xAxis, xAxisTitle, yAxisTitle, datumOn
           style={{ parent: { maxWidth: "60%" } }}>
           <VictoryLegend x={125} y={10}
             orientation="vertical"
-            colorScale={[ "#4d47b4", "#9a31a4"]}
+            colorScale={[ "rgba(77,71,180,.7)", "rgba(154,49,164,.7)"]}
             data={[
               { name: `${datumOneTitle}` }, { name: `${datumTwoTitle}` }
             ]}
             />
           <VictoryAxis
-              label= {xAxisTitle}
-              style={ {axisLabel: {padding: 30}} }
-              tickFormat={(x) => (`${x}`)}/>
-            <VictoryAxis dependentAxis
-              label={yAxisTitle}
-              style={ {axisLabel: {padding: 30}} }
-              tickFormat={(x) => (`${x / 1000}`)} />
-            <VictoryStack colorScale={["#4d47b4", "#9a31a4"]}>
-              <VictoryBar
-                data={filteredDataSet}
-                x={xAxis}
-                y={datumOne}/>
-              <VictoryBar
-                data={filteredDataSet}
-                x={xAxis}
-                y={datumTwo}/>
-            </VictoryStack>
+            label= {xAxisTitle}
+            style={{
+              axisLabel: {padding: 30},
+              tickLabels: {fontSize: 8, padding: 5}
+            }}
+            tickFormat={(x) => (`${x}`)}/>
+          <VictoryAxis dependentAxis
+            label={yAxisTitle}
+            style={{
+              axisLabel: {padding: 30},
+              tickLabels: {fontSize: 8, padding: 5}
+            }}
+            tickFormat={(x) => (`${x / 1000}`)} />
+          <VictoryStack colorScale={["rgba(77,71,180,.7)", "rgba(154,49,164,.7)"]}>
+            <VictoryBar
+              data={filteredDataSet}
+              x={xAxis}
+              y={datumOne}/>
+            <VictoryBar
+              data={filteredDataSet}
+              x={xAxis}
+              y={datumTwo}/>
+          </VictoryStack>
         </VictoryChart>
       </div>
     );
+  } else if (type === "area-chart") {
+    return (
+      <VictoryChart
+        theme={VictoryTheme.material}
+        domain={{ y:domain }}
+        style={{ parent: { maxWidth: "60%" } }}
+        >
+        <VictoryAxis
+          label= {xAxisTitle}
+          style={{
+            axisLabel: {padding: 30},
+            tickLabels: {fontSize: 8, padding: 5}
+          }}
+          tickFormat={(x) => (`${x}`)}/>
+        <VictoryAxis dependentAxis
+          label={yAxisTitle}
+          style={{
+            axisLabel: {padding: 35},
+            tickLabels: {fontSize: 8, padding: 5}
+          }}/>
+        <VictoryArea
+          style={{ data: { fill: "rgba(238, 110, 23, 0.6)" } }}
+          data={dataSet}
+          x={xAxis}
+          y={yAxis}
+          />
+      </VictoryChart>
+    )
   }
 }
 
