e349f322a11b8509f09563b50e48b3967bfd00fb
Addison Cole <addison.cole85@gmail.com>
2019-01-24 02:52:04 -0800

    add welcome page and link to data page upon entering.

--- seattlization/src/components/WelcomeComponent.js
+++ seattlization/src/components/WelcomeComponent.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 import encampment from './Nickelsville.jpg';
 
 import './WelcomeComponent.css';
@@ -14,12 +15,14 @@ class WelcomeComponent extends Component {
 
     return (
       <div className="front-page-container">
-        <div className="box-overlay"></div>
         <div className="gradient-wrap">
           <div className="contrast-wrap">
-            <img alt="Tent Encampmennt, Nickelsville" className="encampment-image" src={encampment} />
+            <img alt="Tent Encampment, Nickelsville" className="encampment-image" src={encampment} />
           </div>
         </div>
+          <h1 className="intro">Before, American cities worried about becoming the next <span className="span-intro">Manhattan</span>...
+            Then, they feared the <span className="span-intro">San Franciso-ization</span> of their towns...Now, comes <span className="span-end">Seattlization.</span></h1>
+          <h4 className="enter-button"><Link className="enter" to="/numbers">Enter</Link></h4>
       </div>
     )
   }
